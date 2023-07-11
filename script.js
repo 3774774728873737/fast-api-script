1c5c278bdff24660feab98b22401d346848fdb70
3774774728873737 <138348862+3774774728873737@users.noreply.github.com>
2023-07-11 14:09:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function handleFileUpload(videoNumber) {
   
   fileInput.onchange = function () {
     var files = fileInput.files;
+    
 
     var videoPlayer = document.getElementById('videoPlayer' + videoNumber);
     var videoThumbnail = document.getElementById('videoThumbnail' + videoNumber);
@@ -11,6 +12,8 @@ function handleFileUpload(videoNumber) {
     videoThumbnail.style.display = 'block';
     videoThumbnail.style.width = '100%'; // Set the width to 450 pixels
     videoThumbnail.style.height = '100%'; // Set the height to 540 pixels
+    videoThumbnail.src = ''; // Clear the previous thumbnail
+    videoThumbnail.alt = ''; // Clear the alt text
     
     // Set placeholder image
     videoThumbnail.src = 'load.png';
@@ -21,7 +24,7 @@ function handleFileUpload(videoNumber) {
     }
     formData.append('videoNumber', videoNumber); // Add the videoNumber to the formData
 
-    fetch('https://fast-api-script-0d0703f4a5dc.herokuapp.com/upload', {
+    fetch('http://localhost:8000/upload', {
       method: 'POST',
       body: formData,
     })
@@ -45,6 +48,34 @@ function handleFileUpload(videoNumber) {
   fileInput.click();
 }
 
+
+function handleAudioUpload() {
+  var audioInput = document.getElementById('audioInput');
+  var audioFile = audioInput.files[0];
+
+  var formData = new FormData();
+  formData.append('file', audioFile);
+
+  fetch('http://localhost:8000/upload-audio', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message); // Display the message
+      // Perform any desired actions with the response from the server
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+// Function to upload the audio file
+function uploadAudio() {
+  var audioInput = document.getElementById('audioInput');
+  audioInput.onchange = handleAudioUpload;
+  audioInput.click();
+}
 
 // Function to handle file upload for video 2 and video 3
 function chooseFile(videoNumber) {
