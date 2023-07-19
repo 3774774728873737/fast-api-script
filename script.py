57b3a37e19edfca76cf2ad8ea860b7333469f921
3774774728873737 <138348862+3774774728873737@users.noreply.github.com>
2023-07-20 02:42:58 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,36 +1,92 @@
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import JSONResponse, FileResponse
 import cv2
+import subprocess
 from fastapi.middleware.cors import CORSMiddleware
 from moviepy.editor import VideoFileClip, clips_array
+from moviepy.editor import concatenate_videoclips
+from typing import List
 from moviepy.editor import *
 import os
-import base64
-import uvicorn
-from typing import List
-import subprocess
-import time
-import random
-import string
+from moviepy.video.fx.all import crop
+
+
+
 global audioname
-
-
 audioname = None
+
+
+
+def resize_video(clip, width, height):
+    # Determine the aspect ratio of the input clip
+    aspect_ratio = clip.w / clip.h
+    
+    # If the aspect ratio of the input clip is greater than the desired aspect ratio
+    # it means that the clip is wider and we need to crop it horizontally
+    if aspect_ratio > width / height:
+        new_width = int(height * aspect_ratio)
+        new_height = height
+    else:
+        new_width = width
+        new_height = int(width / aspect_ratio)
+
+    # Resize the clip to the new dimensions
+    clip = clip.resize((new_width, new_height))
+
+    # Crop the clip to the desired dimensions
+    clip = crop(clip, width=width, height=height, x_center=clip.w / 2, y_center=clip.h / 2)
+
+    return clip
 
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Update this with your desired origins
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 
+
+
+
+@app.post("/upload")
+async def upload_file(files: List[UploadFile] = File(...), videoNumber: int = Form(...)):
+    video_clips = []
+
+    for file in files:
+        temp_file_name = f"temp{file.filename}"
+        with open(temp_file_name, "wb") as f:
+            f.write(await file.read())
+        width = 426
+        height = 720
+        video_clips.append(resize_video(VideoFileClip(temp_file_name), width, height))
+        # video_clips.append(VideoFileClip(temp_file_name))
+    
+    # concatenate videos into one
+    final_clip = concatenate_videoclips(video_clips, method='compose')
+    final_clip_name = f"video{videoNumber}.mp4"
+    final_clip.write_videofile(final_clip_name)
+
+    # Generate thumbnail image for the uploaded video
+    video_capture = cv2.VideoCapture(final_clip_name)
+    success, frame = video_capture.read()
+    if success:
+        thumbnail_path = f"thumbnail{videoNumber}.jpg"
+        cv2.imwrite(thumbnail_path, frame)
+    else:
+        thumbnail_path = None
+
+    return JSONResponse({"message": "Videos uploaded successfully", "imagePath": thumbnail_path})
+
+
+
 @app.post("/upload-audio")
 async def upload_audio(file: UploadFile = File(...)):
     # Save the uploaded audio file
+
     i = 1
     while True:
         if os.path.exists(f"audio{i}.mp3"):
@@ -40,141 +96,65 @@ async def upload_audio(file: UploadFile = File(...)):
 
     with open(f"audio{i}.mp3", "wb") as f:
         f.write(await file.read())
-
+    
     global audioname
     audioname = f"audio{i}.mp3"
-    time.sleep(4)
-
-    return {"message": "Videos uploaded successfully"}
-
-
-@app.post("/upload-videos")
-async def upload_videos(files: List[UploadFile] = File(...)):
-    print(files)
-    i = 1
-    for file in files:
-        with open(f"video{i}.mp4", "wb") as f:
-            f.write(await file.read())
-        i+=1
-
-    return {"message": "Videos uploaded successfully"}
-
-
-@app.post("/upload")
-async def upload_file(file: UploadFile = File(...), videoNumber: int = Form(...)):
-    with open(f"video{videoNumber}.mp4", "wb") as f:
-        f.write(await file.read())
-
-    # Generate thumbnail image for the uploaded video
-    video_capture = cv2.VideoCapture(f"video{videoNumber}.mp4")
-    success, frame = video_capture.read()
-    if success:
-        # Save the thumbnail as a temporary file
-        thumbnail_path = f"thumbnail{videoNumber}23.jpg"
-        cv2.imwrite(thumbnail_path, frame)
-
-        # Read the thumbnail image and convert it to base64
-        with open(thumbnail_path, "rb") as thumbnail_file:
-            thumbnail_data = thumbnail_file.read()
-            thumbnail_base64 = base64.b64encode(thumbnail_data).decode("utf-8")
-
-    else:
-        thumbnail_base64 = None
-
-    return JSONResponse({"message": "Video uploaded successfully", "imagePath": thumbnail_base64})
 
 
 
-def generate_unique_filename():
-    timestamp = str(int(time.time()))
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
-    unique_filename = timestamp + '_' + random_string
-    return unique_filename
 
 
-
-@app.post("/combine")
-async def combine_videos(files: List[UploadFile] = File(...), audio: UploadFile = File(None)):
-
-
-
-    print(len(files))
-    file1 = generate_unique_filename()
-    audionames = generate_unique_filename()
-    outputname = generate_unique_filename()
-
-
-    i = 1
-    for i, file in enumerate(files, start=1):
-        print(file1)
-        with open(f"{file1}{i}.mp4", "wb") as f:
-            f.write(await file.read())
-        i+=1
-    count = 0
-
-    if audio is not None:
-        # Save the uploaded audio file
-        with open(f"{audionames}.mp3", "wb") as f:
-            f.write(await audio.read())
-            audios = f"-i {audionames}.mp3"
-            count = count + 1
-
-    else:
-        audios = ""
-        print("no audio")
-
+    
+@app.get("/combine")
+async def combine_videos():
 
 
     global audioname
 
-    length = 6
+    
+    clip1 = VideoFileClip("video1.mp4")
+    clip2 = VideoFileClip("video2.mp4")
+    clip3 = VideoFileClip("video3.mp4")
+
+    width = 426
+    height = 720
+
+    clip1 = resize_video(VideoFileClip("video1.mp4"), width, height)
+    clip2 = resize_video(VideoFileClip("video2.mp4"), width, height)
+    clip3 = resize_video(VideoFileClip("video3.mp4"), width, height)
+    combined = clips_array([[clip1 ,clip2,clip3]])
+
+    output_width = 1280
+    output_height = 720    
 
 
-    audio_merge = ";"
-    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video1.mp4"]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode == 0 and result.stdout.strip() == 'audio':
-        audio_merge = audio_merge + "[0:a]"
-        count = count + 1
-    else:
-        print("no audio")
-        pass
 
-    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video2.mp4"]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode == 0 and result.stdout.strip() == 'audio':
-        audio_merge = audio_merge + "[1:a]"
-        count = count + 1
-    else:
-        pass
-
-    command = ['ffprobe', '-v', 'error', '-select_streams', 'a:0', '-show_entries', 'stream=codec_type', '-of', 'default=noprint_wrappers=1:nokey=1', "video3.mp4"]
-    result = subprocess.run(command, capture_output=True, text=True)
-    if result.returncode == 0 and result.stdout.strip() == 'audio':
-        audio_merge = audio_merge + "[2:a]"
-        count = count + 1
-    else:
-        pass
+    combined2 = combined.resize((output_width, output_height))
 
 
-    if count > 0:
-        audio_merge = audio_merge + f"amerge=inputs={count}[a]"
-        maping = "-map \"[a]\""
+  
+    if audioname != None:
+        # Trim audio clip to match the duration of the video
+        audio = AudioFileClip(audioname).subclip(0, combined2.duration)
+        combined2 = combined2.set_audio(audio)
+        combined2.write_videofile("test.mp4")
+        audio.reader.close_proc()
 
     else:
-        audio_merge = ""
-        maping = ""
-        
-    print(audio_merge)
-
-    # run a single command
-    command = f"""ffmpeg -y -i {file1}1.mp4 -i {file1}2.mp4 -i {file1}3.mp4 {audios} -vsync 2 -filter_complex "[0:v]scale=426:720[v0];[1:v]scale=426:720[v1];[2:v]scale=426:720[v2];[v0][v1][v2]hstack=3,scale=1280:720[v]{audio_merge} " -map "[v]" {maping} -c:v libx264 -crf 23 -preset veryfast -c:a libmp3lame -b:a 128k -t {length} {outputname}.mp4"""
-    subprocess.run(command, shell=True)
-
-    return FileResponse(f"{outputname}.mp4", media_type="video/mp4")
+        combined2.write_videofile("test.mp4")
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
 
-# changedasdasdsaasd
+    audioname = None
+    audioname = None
+    clip1.reader.close()
+    clip2.reader.close()
+    clip3.reader.close()
+
+
+
+    return FileResponse("test.mp4", media_type="video/mp4")
+    return send_file('test.mp4', as_attachment=True)
+
+
+
